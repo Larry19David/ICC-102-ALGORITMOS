381bac19cb6d91aa2c90379347d388aebe8c6585
LarryDavid19 <20181553ce.edu.pucmm.edu.do>
2019-05-14 18:39:13 -0400

    Create ejercicio 1 parte 1.py
    ejercicio realizado en Pseint

---
+++ ejercicio 1 parte 1.py
@@ -0,0 +1,6 @@
+print ("digite su nombre:")
+nombre = input()
+print("digite su edad: ")
+edad = int(input())
+print("hola, " + nombre + ", usted tiene " + str(edad) + " años, y en diez años tendra " + str(edad+10))
+
